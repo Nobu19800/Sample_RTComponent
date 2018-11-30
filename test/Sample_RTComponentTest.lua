eae6c06f95456a4d03efd9e3ffb8c2e8a99f214c
Nobuhiko Miyamoto <TyouKyozyakuTaisitu@yahoo.co.jp>
2018-11-30 09:41:31 +0900

    ファイルのアップロード

---
+++ test/Sample_RTComponentTest.lua
@@ -0,0 +1,248 @@
+---------------------------------
+--! @file Sample_RTComponentTest.py
+--! @brief ModuleDescription
+--! @date $Date$
+
+---------------------------------
+
+local openrtm  = require "openrtm"
+
+
+
+-- Import Service implementation class
+-- <rtc-template block="service_impl">
+
+
+-- </rtc-template>
+
+
+-- This module's spesification
+-- <rtc-template block="module_spec">
+local sample_rtcomponenttest_spec = {["implementation_id"]="Sample_RTComponentTest", 
+		 ["type_name"[="Sample_RTComponentTest", 
+		 ["description"]="ModuleDescription",
+		 ["version"]="1.0.0",
+		 ["vendor"]="VenderName",
+		 ["category"]="Category",
+		 ["activity_type"]="STATIC",
+		 ["max_instance"]="1",
+		 ["language"]="Lua",
+		 ["lang_type"]="SCRIPT",
+		 ""}
+-- </rtc-template>
+
+-- @class Sample_RTComponentTest
+-- @brief ModuleDescription
+local Sample_RTComponentTest = {}
+Sample_RTComponentTest.new = function(manager)
+	local obj = {}
+	setmetatable(obj, {__index=openrtm.RTObject.new(manager)})
+
+
+
+
+
+
+
+
+	-- initialize of configuration-data.
+	-- <rtc-template block="init_conf_param">
+
+	-- </rtc-template>
+		 
+	--
+	-- The initialize action (on CREATED->ALIVE transition)
+	-- formaer rtc_init_entry()
+	--
+	-- @return RTC::ReturnCode_t
+	--
+	--
+	function obj:onInitialize()
+		-- Bind variables and configuration variable
+
+		-- Set OutPort buffers
+
+		-- Set InPort buffers
+
+		-- Set service provider to Ports
+
+		-- Set service consumers to Ports
+
+		-- Set CORBA Service Ports
+
+		return self._ReturnCode_t.RTC_OK
+	end
+
+	--	--
+	--	-- The finalize action (on ALIVE->END transition)
+	--	-- formaer rtc_exiting_entry()
+	--	--
+	--	-- @return RTC::ReturnCode_t
+	--
+	--	--
+	--	function obj:onFinalize()
+	--	
+	--		return self._ReturnCode_t.RTC_OK
+	--	end
+
+	--	--
+	--	-- The startup action when ExecutionContext startup
+	--	-- former rtc_starting_entry()
+	--	--
+	--	-- @param ec_id target ExecutionContext Id
+	--	--
+	--	-- @return RTC::ReturnCode_t
+	--	--
+	--	--
+	--	function obj:onStartup(ec_id)
+	--	
+	--		return self._ReturnCode_t.RTC_OK
+	--	end
+
+	--	--
+	--	-- The shutdown action when ExecutionContext stop
+	--	-- former rtc_stopping_entry()
+	--	--
+	--	-- @param ec_id target ExecutionContext Id
+	--	--
+	--	-- @return RTC::ReturnCode_t
+	--	--
+	--	--
+	--	function obj:onShutdown(ec_id)
+	--	
+	--		return self._ReturnCode_t.RTC_OK
+	--	end
+
+	--	--
+	--	-- The activated action (Active state entry action)
+	--	-- former rtc_active_entry()
+	--	--
+	--	-- @param ec_id target ExecutionContext Id
+	--	--
+	--	-- @return RTC::ReturnCode_t
+	--	--
+	----
+	--function obj:onActivated(ec_id)
+	--
+	--	return self._ReturnCode_t.RTC_OK
+	--end
+
+	--	--
+	--	-- The deactivated action (Active state exit action)
+	--	-- former rtc_active_exit()
+	--	--
+	--	-- @param ec_id target ExecutionContext Id
+	--	--
+	--	-- @return RTC::ReturnCode_t
+	--	--
+	--	--
+	--	function obj:onDeactivated(ec_id)
+	--	
+	--		return self._ReturnCode_t.RTC_OK
+	--	end
+
+	--	--
+	--	-- The execution action that is invoked periodically
+	--	-- former rtc_active_do()
+	--	--
+	--	-- @param ec_id target ExecutionContext Id
+	--	--
+	--	-- @return RTC::ReturnCode_t
+	--	--
+	--	--
+	--	function obj:onExecute(ec_id)
+	--	
+	--	return self._ReturnCode_t.RTC_OK
+	--end
+
+	--	--
+	--	-- The aborting action when main logic error occurred.
+	--	-- former rtc_aborting_entry()
+	--	--
+	--	-- @param ec_id target ExecutionContext Id
+	--	--
+	--	-- @return RTC::ReturnCode_t
+	--	--
+	--	--
+	--	function obj:onAborting(ec_id)
+	--	
+	--	return self._ReturnCode_t.RTC_OK
+	--end
+
+	--	--
+	--	-- The error action in ERROR state
+	--	-- former rtc_error_do()
+	--	--
+	--	-- @param ec_id target ExecutionContext Id
+	--	--
+	--	-- @return RTC::ReturnCode_t
+	--	--
+	--	--
+	--	function obj:onError(ec_id)
+	--	
+	--		return self._ReturnCode_t.RTC_OK
+	--	end
+
+	--	--
+	--	-- The reset action that is invoked resetting
+	--	-- This is same but different the former rtc_init_entry()
+	--	--
+	--	-- @param ec_id target ExecutionContext Id
+	--	--
+	--	-- @return RTC::ReturnCode_t
+	--	--
+	--	--
+	--	function obj:onReset(ec_id)
+	--	
+	--		return self._ReturnCode_t.RTC_OK
+	--	end
+
+	--	--
+	--	-- The state update action that is invoked after onExecute() action
+	--	-- no corresponding operation exists in OpenRTm-aist-0.2.0
+	--	--
+	--	-- @param ec_id target ExecutionContext Id
+	--	--
+	--	-- @return RTC::ReturnCode_t
+	--	--
+
+	--	--
+	--	function obj:onStateUpdate(ec_id)
+	--	
+	--		return self._ReturnCode_t.RTC_OK
+	--	end
+
+	--	--
+	--	-- The action that is invoked when execution context's rate is changed
+	--	-- no corresponding operation exists in OpenRTm-aist-0.2.0
+	--	--
+	--	-- @param ec_id target ExecutionContext Id
+	--	--
+	--	-- @return RTC::ReturnCode_t
+	--	--
+	--	--
+	--	function obj:onRateChanged(ec_id)
+	--	
+	--		return self._ReturnCode_t.RTC_OK
+	--	end
+	return obj
+end
+
+
+local MyModuleInit = function(manager)
+    Sample_RTComponentTest.Init(manager)
+
+    -- Create a component
+    local comp = manager:createComponent("Sample_RTComponentTest")
+end
+
+if openrtm.Manager.is_main() then
+	local manager = openrtm.Manager
+	manager:init(arg)
+	manager:setModuleInitProc(MyModuleInit)
+	manager:activateManager()
+	manager:runManager()
+else
+	return Sample_RTComponentTest
+end
+
